5d833095777d282f37ac620cb9bd78e1cc7856de
Davide Carpini <carpini.davide@gmail.com>
2024-11-05 18:53:27 +0100

    fix: finish app and add video

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playerCard = "back"
+    @State var cpuCard = "back"
+    @State var playerScore = 0
+    @State var cpuScore = 0
     var body: some View {
         ZStack {
             Image("background-wood-cartoon").resizable()
@@ -17,28 +21,49 @@ struct ContentView: View {
                 Image("logo")
                 HStack {
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
-                Image("button")
+                Button{
+                    deal()
+                } label: {
+                    Image("button")
+                }
                 HStack {
                     Spacer()
                     VStack(spacing: 10) {
                         Text("Player")
-                        Text("0").font(.largeTitle).bold()
+                        Text(String(playerScore)).font(.largeTitle).bold()
                     }
                     Spacer()
                     VStack(spacing: 10) {
                         Text("CPU")
-                        Text("0").font(.largeTitle).bold()
+                        Text(String(cpuScore)).font(.largeTitle).bold()
                     }
                     Spacer()
                 }.foregroundStyle(Color(.white))
             }.padding()
         }
         
+    }
+    
+    func deal() {
+        let playerCardValue = Int.random(in: 2...14)
+        // remove the playerCard from the possible values for the cpuCard
+        let possibleCpuCardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].filter { $0 != playerCardValue }
+        let cpuCardValue = possibleCpuCardValues.randomElement()!
+        // Randomize the player card
+        playerCard = "card" + String(playerCardValue)
+        // Randomize the cpu card
+        cpuCard = "card" + String(cpuCardValue)
+        // Update the scores
+        if playerCardValue > cpuCardValue {
+            playerScore += 1
+        } else {
+            cpuScore += 1
+        }
     }
 }
 
