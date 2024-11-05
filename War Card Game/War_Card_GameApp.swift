9a4c6d090b866843e18c0f1e541fc046a0b0d116
Davide Carpini <carpini.davide@gmail.com>
2024-11-05 14:16:47 +0100

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Davide Carpini on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
